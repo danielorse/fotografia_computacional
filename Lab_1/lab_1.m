a5e738fb050d65aa9db70eef4282a8219ce47906
danielorse <danielorse06pro@gmail.com>
2026-02-16 18:34:50 +0100

    implementada la función de los 3 vecinos

--- Lab_1/lab_1.m
+++ Lab_1/lab_1.m
@@ -126,7 +126,91 @@ zona_ojoH = result_hilbert(110:220, 320:440);
 imshow(zona_ojoH);
 title('Zona del ojo - hilbert');
 
+% funcion para repatir el error a sus 3 vecinos
+function im = dither_3vecinos(im)
+    % Convertimos a valores entre 0 y 1 (im2double)
+    im = im2double(im);
+    
+    % Recorremos la imagen pixel a pixel
+    [filas, columnas] = size(im);
+    
+    for fila = 1:filas
+        % si la fila es impar recorremos de izquierda a derecha
+        if (mod(fila,2)) == 1
+            for columna = 1:columnas
+                % Obtenemos el valor del pixel actual
+                V = im(fila, columna);
+                
+                % Comparamos V con 0.5
+                if V >= 0.5
+                    im(fila, columna) = 1;
+                else
+                    im(fila, columna) = 0;
+                end
+                
+                % Calculamos el error cometido para usarlo con el siguiente  
+                e = V - im(fila, columna);
+                
+                % Difundimos el error a 3 vecinos
+                % Derecha (4/8)
+                if columna+1 <= columnas
+                    im(fila, columna+1) = im(fila, columna+1) + e*(4/8);
+                end
+                % Abajo (3/8)
+                if fila+1 <= filas 
+                    im(fila+1, columna) = im(fila+1, columna) + e*(3/8);
+                end
+                % Abajo-derecha (1/8)
+                if (fila+1 <= filas) && (columna+1 <= columnas)
+                    im(fila+1, columna+1) = im(fila+1, columna+1) + e*(1/8);
+                end
+            end
+        else
+            % si la fila es par, recorremos de derecha a izquierda
+            for columna = columnas:-1:1
+                % Obtenemos el valor del pixel actual
+                V = im(fila, columna);
+                
+                % Comparamos V con 0.5
+                if V >= 0.5
+                    im(fila, columna) = 1;
+                else
+                    im(fila, columna) = 0;
+                end
+                
+                % Calculamos el error cometido
+                e = V - im(fila, columna);
+                
+                % Difundimos el error a 3 vecinos
+                % Izquierda (4/8)
+                if columna-1 >= 1
+                    im(fila, columna-1) = im(fila, columna-1) + e*(4/8);
+                end
+                % Abajo (3/8)
+                if fila+1 <= filas
+                    im(fila+1, columna) = im(fila+1, columna) + e*(3/8);
+                end
+                % Abajo-izquierda (1/8)
+                if (fila+1 <= filas) && (columna-1 >= 1)
+                    im(fila+1, columna-1) = im(fila+1, columna-1) + e*(1/8);
+                end
+            end
+        end
+    end
+end % end de la funion
 
+% Mostramos la imagen obtenida tras aplicar el error a 3 vecinos
+im = imread('FC_PROY1\imagen.png');
+result3vecinos = dither_3vecinos(im);
+figure
+imshow(result3vecinos);
+title('Resultado de aplicar error a los 3 vecinos');
+
+% motramos la zona del ojo
+figure
+zona_ojo3V = result3vecinos(110:220, 320:440);
+imshow(zona_ojo3V);
+title('Zona del ojo - 3 vecinos');
 
 
 
